cc1317989e524a5738895bb676681bbe95153ed2
dhruba.goswami <43171362+dhruba001@users.noreply.github.com>
2023-12-23 08:19:55 +0530

    diffent element is rendered

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
+const parent = React.createElement("div", { id: "heading" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+  ]),
+
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+  ]),
+]);
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "hello from react"
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
